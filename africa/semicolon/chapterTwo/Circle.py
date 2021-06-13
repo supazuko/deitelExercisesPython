f5867553f2e6805b3e4bae3b08cab89a709b3554
Francis Obi <supazuko@gmail.com>
2021-06-13 04:42:10 +0100

    chapter two exercise

---
+++ africa/semicolon/chapterTwo/Circle.py
@@ -0,0 +1,18 @@
+radius = 2
+pi = 3.14159
+
+
+def diameter():
+    return 2 * radius
+
+
+def circumference():
+    return 2 * pi * radius
+
+
+def area():
+    return pi * (radius * radius)
+
+
+print(diameter(), circumference(), area())
+
