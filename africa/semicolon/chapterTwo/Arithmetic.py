30f62fe290d033e5b531e7e1856758833e88b7e9
Francis Obi <supazuko@gmail.com>
2021-06-13 04:44:29 +0100

    chapter two exercise

---
+++ africa/semicolon/chapterTwo/Arithmetic.py
@@ -0,0 +1,30 @@
+firstOperand = 27.5
+secondOperand = 2
+
+
+def add():
+    return firstOperand + secondOperand
+
+
+def subtract():
+    return firstOperand - secondOperand
+
+
+def multiply():
+    return firstOperand * secondOperand
+
+
+def division():
+    return firstOperand / secondOperand
+
+
+def floor_division():
+    return firstOperand // secondOperand
+
+
+def exponential():
+    return firstOperand ** secondOperand
+
+
+print(add(), subtract(), multiply(), division(), floor_division(), exponential())
+
