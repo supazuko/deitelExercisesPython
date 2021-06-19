5df7b6ecbf3e413a565101e015db4f3a58b7df98
Francis Obi <supazuko@gmail.com>
2021-06-19 05:27:51 +0100

    chapter two exercise

---
+++ africa/semicolon/chapterTwo/OddOrEven.py
@@ -0,0 +1,10 @@
+def odd_or_even():
+    number = int(input("Enter a number: "))
+    if number % 2 == 0:
+        print("number is even!")
+    else:
+        print("number is odd!")
+
+
+print(odd_or_even())
+
